b1b4ffbb29608e52838e05b361c804a7027c8596
João <jpedrocs12@gmail.com>
2022-08-03 10:30:27 -0300

    Novas atualizações

--- Home/script.js
+++ Home/script.js
@@ -1,1 +1,16 @@
-console.log(window.visualViewport.width)
+var iconMenu = document.querySelectorAll('.material-icons')
+
+iconMenu[0].addEventListener('click', ()=>{
+    menu = document.getElementById('menu')
+
+    if(menu.classList.contains('hide')){
+        menu.classList.add('show')
+        menu.classList.remove('hide')
+    }
+    if(menu.classList.contains('show')){
+        menu.classList.add('hide')
+        menu.classList.remove('show')
+    }
+
+
+})
