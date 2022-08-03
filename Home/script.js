9e7ac41ad56ca1bc6f3b91208d105a88d70366ec
John <jpedrocs12@gmail.com>
2022-08-03 16:41:53 -0300

    pc positivo

--- Home/script.js
+++ Home/script.js
@@ -1,1 +1,10 @@
-console.log('Hello')
+var altura = window.screen.height
+var largura = window.screen.width
+
+var header = document.getElementsByClassName("header-home")
+
+function changeHeader(largura){
+    if(largura<"500px"){
+        console.log("hello")
+    }
+}
