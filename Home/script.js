cc7a71c29d51707c0a67b7f285c094a044295f20
John <jpedrocs12@gmail.com>
2022-09-22 14:16:39 -0300

    Update sec-main

--- Home/script.js
+++ Home/script.js
@@ -2,15 +2,6 @@ var iconMenu = document.querySelectorAll('.material-icons')
 
 iconMenu[0].addEventListener('click', ()=>{
     menu = document.getElementById('menu')
-
-    if(menu.classList.contains('hide')){
-        menu.classList.add('show')
-        menu.classList.remove('hide')
-    }
-    if(menu.classList.contains('show')){
-        menu.classList.add('hide')
-        menu.classList.remove('show')
-    }
-
+    console.log(menu)
 
 })
